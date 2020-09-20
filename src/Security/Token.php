d6a5ef684b88e44b1ee514dc46aa619bdc4d5a49
Didier <dmoindreauu@gmail.com>
2020-09-20 14:58:44 +0200

    bug :generated Token string length is double than expected

--- src/Security/Token.php
+++ src/Security/Token.php
@@ -3,19 +3,29 @@
 
 namespace Core\Security;
 
-
+/**
+ * Class Token
+ * @package Core\Security
+ */
 class Token
 {
     private int $length;
     private string $token;
 
+    /**
+     * Token constructor.
+     * @param int $length 16 is the minimal recommended value
+     */
     public function __construct(int $length = 16)
     {
-        $this->length = $length;
+        $this->length = $length/2;
         // $token = bin2hex(openssl_random_pseudo_bytes($this->length));
         $this->token = bin2hex(random_bytes($this->length));
     }
 
+    /**
+     * @return string
+     */
     public function __toString()
     {
         return $this->token;
