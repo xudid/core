efe3d4faec11a806e2c220f567667e319c54277e
Didier Moindreau <dmoindreau@gmail.com>
2021-09-11 23:39:37 +0200

    Add Hydrate trait

---
+++ src/Traits/Hydrate.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Core\Traits;
+
+trait Hydrate
+{
+    public static function hydrate(array $data)
+    {
+        $object = new static();
+
+        $methods = get_class_methods(__CLASS__);
+        $keys = array_keys($data);
+        foreach ($keys as $key) {
+            $key = strtolower($key);
+            $setter = 'set'.ucfirst($key);
+            $value = $data[$key];
+            if (in_array($setter, $methods)) {
+                $object->$setter($value);
+            }
+        }
+        return $object;
+    }
+}
