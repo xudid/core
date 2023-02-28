84d5112dfd763420d01beb8a4e0f9d44b0d5d4e2
Didier Moindreau <dmoindreau@gmail.com>
2023-02-28 18:32:12 +0100

    ActionInterface

--- src/Contracts/ActionInterface.php
+++ src/Contracts/ActionInterface.php
@@ -2,7 +2,13 @@
 
 namespace Core\Contracts;
 
-interface ActionInterface
-{
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
 
+interface ActionInterface extends MiddlewareInterface
+{
+    public function __construct(ResponseInterface $response, ...$properties);
+
+    public function handle(ServerRequestInterface $request): ResponseInterface;
 }
