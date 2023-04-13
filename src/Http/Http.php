760284f73e9c4ba65c40b7646503a77e8547557e
Didier Moindreau <dmoindreau@gmail.com>
2023-04-13 14:03:43 +0200

    Http add redirect method

--- src/Http/Http.php
+++ src/Http/Http.php
@@ -11,4 +11,11 @@ abstract class Http
     {
         send($response);
     }
+
+    public static function redirect(ResponseInterface $response, $url)
+    {
+        $response = $response->withStatus(302)->withHeader('Location', $url);
+        static::send($response);
+        exit();
+    }
 }
