31d7361fba5d5dfa00fda7308cc5b466b6ad3c60
Didier Moindreau <dmoindreau@gmail.com>
2023-04-29 18:23:50 +0200

    Token add isExpired method

--- src/Security/Token.php
+++ src/Security/Token.php
@@ -3,6 +3,8 @@
 
 namespace Core\Security;
 
+use DateTimeImmutable;
+
 /**
  * Class Token
  * @package Core\Security
@@ -12,21 +14,27 @@ class Token
     private int $length;
     private string $token;
 
+    private DateTimeImmutable $creationDate;
+
     /**
      * Token constructor.
      * @param int $length 16 is the minimal recommended value
      */
     public function __construct(int $length = 16)
     {
-        $this->length = $length/2;
+        $this->length = $length / 2;
         // $token = bin2hex(openssl_random_pseudo_bytes($this->length));
         $this->token = bin2hex(random_bytes($this->length));
+        $this->creationDate = new DateTimeImmutable();
     }
 
-    /**
-     * @return string
-     */
-    public function __toString()
+    public function isExpired(int $validityMinutes): bool
+    {
+        $expirationDate = $this->creationDate->modify('+' . $validityMinutes . ' minutes');
+        return $expirationDate < new DateTimeImmutable();
+    }
+
+    public function __toString(): string
     {
         return $this->token;
     }
